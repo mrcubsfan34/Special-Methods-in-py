dc2abf2139536fffd86e0aa98aefc2aacba01f86
Michael Truss <badmoonrising31@gmail.com>
2018-11-27 10:31:26 -0600

    Create special_methods.py
    Here is the file!

---
+++ special_methods.py
@@ -0,0 +1,31 @@
+from copy import copy
+
+class Human:
+   def __init__(self, first, last, age):
+      self.first = first
+      self.last = last
+      self.age = age
+      
+   def __repr__(self):
+      return f"Human named {self.first} {self.last} aged {self.age}"
+      
+   def __len__(self):
+      return self.age
+      
+   def __add__(self, other):
+      if isinstance(other, Human):
+          return Human(first='Newborn', last=self.last, age=0)
+      return "You can't add that!"
+      
+   def __mul__(self, other):
+      if isinstance(other, int):
+          return [copy(self) for i in range(other)]
+      return "Can't multiply!"
+      
+   j = Human("Jenny", "Larsen", 47)
+   k = Human("Kevin", "Jones", 49)
+   
+   triplets = (k + j) * 3
+   print(triplets)
+          
+   
